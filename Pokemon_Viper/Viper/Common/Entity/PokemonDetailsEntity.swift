417579a543ef66eb7929ec12a9f8dcfb0e5cf53a
Gabriel <moraesnicol@gmail.com>
2021-07-01 08:37:05 -0300

    pokemon details entity ok

---
+++ Pokemon_Viper/Viper/Common/Entity/PokemonDetailsEntity.swift
@@ -0,0 +1,21 @@
+//
+//  PokemonDetailsEntity.swift
+//  Pokemon_Viper
+//
+//  Created by Gabriel on 01/07/21.
+//
+
+import Foundation
+
+struct PokemonDetailsEntity {
+    
+    var name: String = ""
+    var order: Int = 0
+    var id: Int = 0
+    var weight: Int = 0
+    var height: Int = 0
+    var baseExperience: Int = 0
+    var image: String = ""
+    var types: [String] = []
+    var mainMove: String = ""
+}
